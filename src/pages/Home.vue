a995d434bdfc030d76d3202f88662ad909ce551f
Dindz <dindoquitor@gmail.com>
2022-04-26 22:08:54 +0800

    Added more section for front page.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -141,6 +141,156 @@
     </div>
     <ContactForm />
   </div>
+  <!-- Idaho Workers Section -->
+  <div class="idaho-workers-section">
+    <div class="container">
+      <div class="idaho-row-1">
+        <div class="idaho-title-desc">
+          <h2>Idaho Workers Compensation</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+        </div>
+        <div class="idaho-btn">
+          <button>contact us</button>
+        </div>
+      </div>
+      <div class="idaho-row-2">
+        <div class="idaho-img-left">
+          <img src="../assets/images/are-you-tired.png" alt="" srcset="" />
+          <div class="idaho-img-left-desc">
+            <p>
+              Are you tired of dealing with the workers compensation adjusters?
+            </p>
+            <h3>read more</h3>
+          </div>
+        </div>
+        <div class="idaho-img-right">
+          <div class="idaho-img-right-top">
+            <img
+              src="../assets/images/have-they-stopped-you.png"
+              alt=""
+              srcset=""
+            />
+            <div class="idaho-img-right-top-desc">
+              <p>Have they stopped your benefits?</p>
+              <h3>read more</h3>
+            </div>
+          </div>
+          <div class="idaho-img-right-bottom">
+            <img
+              src="../assets/images/are-you-getting-everything.png"
+              alt=""
+              srcset=""
+            />
+            <div class="idaho-img-right-bottom-desc">
+              <p>Are you getting everything you are entitled to?</p>
+              <h3>read more</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Our Client Reviews Section -->
+  <div class="container">
+    <div class="our-focus-section our-client-review">
+      <h1>Our Clients Reviews</h1>
+      <div class="cards">
+        <div class="col">
+          <div class="col-img our-client-review-img">
+            <img src="../assets/images/John-1.png" alt="" srcset="" />
+          </div>
+          <div class="col-title col-1 our-client-review-desc">
+            <h2>John Doe</h2>
+            <p>Client</p>
+          </div>
+          <div class="col-desc">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipi scing elit. Integer
+              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+              Nulla quis sem at nibh elementum imperdiet. Duis sagitt is ipsum.
+              Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
+              massa. Vestibul um lacinia arcu eget nulla.
+            </p>
+          </div>
+        </div>
+        <div class="col">
+          <div class="col-img our-client-review-img">
+            <img src="../assets/images/John-2.png" alt="" srcset="" />
+          </div>
+          <div class="col-title col-2 our-client-review-desc">
+            <h2>John Doe</h2>
+            <p>client</p>
+          </div>
+
+          <div class="col-desc">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipi scing elit. Integer
+              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+              Nulla quis sem at nibh elementum imperdiet. Duis sagitt is ipsum.
+              Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
+              massa. Vestibul um lacinia arcu eget nulla.
+            </p>
+          </div>
+        </div>
+        <div class="col">
+          <div class="col-img our-client-review-img">
+            <img src="../assets/images/John-3.png" alt="" srcset="" />
+          </div>
+          <div class="col-title col-3 our-client-review-desc">
+            <h2>John Doe</h2>
+            <p>Client</p>
+          </div>
+          <div class="col-desc">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipi scing elit. Integer
+              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+              Nulla quis sem at nibh elementum imperdiet. Duis sagitt is ipsum.
+              Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
+              massa. Vestibul um lacinia arcu eget nulla.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="hr-line"><hr /></div>
+      <div class="three-line">
+        <div class="line-three"><hr /></div>
+        <div class="line-two"><hr /></div>
+        <div class="line-three"><hr /></div>
+      </div>
+    </div>
+  </div>
+  <!-- Have Pre-existing Conditions Section -->
+  <div class="container">
+    <div class="pre-exixting-conditions-section">
+      <div class="pre-ex-cond-img">
+        <img src="../assets/images/Have Pre-existing Conditions.png" alt="" />
+      </div>
+      <div class="pre-ex-title">
+        <h3>Have Pre-existing Conditions</h3>
+        <div class="pre-ex-line-circle">
+          <div class="pre-ex-line">
+            <img src="../assets/images/Rectangle 51.png" alt="" />
+          </div>
+          <div class="pre-ex-circle">
+            <img src="../assets/images/Ellipse 2.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="pre-ex-desc">
+        <p>
+          Have pre-existing conditions and now a new injury and unsure if you
+          will be able to return to work. You may have a case for lifetime
+          benefits through the idaho special indemnity fund.
+        </p>
+      </div>
+    </div>
+  </div>
+  <!-- Our Blog -->
+  <div class="ourblog"></div>
 </template>
 
 <script setup lang="ts">
