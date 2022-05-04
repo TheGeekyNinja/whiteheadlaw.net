6df88b95ba1cf9195c519a86f345e549cb28e749
dindoquitor <dindoquitor@gmail.com>
2022-05-04 11:22:45 +0800

    Added validation to forms.

--- src/components/Contact-form.vue
+++ src/components/Contact-form.vue
@@ -1,22 +1,85 @@
 <template>
   <div class="container">
-    <form class="contact-form">
+    <Form ref="form" @submit="onSubmit" class="contact-form">
       <title class="form-title">Drop us a line!</title>
-      <input type="text" placeholder="First Name" />
-      <input type="text" placeholder="Last Name" />
-      <input type="email" placeholder="Email" />
-      <input type="tel" placeholder="Contact Number" />
-      <textarea
+      <Field
+        name="firstName"
+        type="text"
+        placeholder="First Name"
+        :rules="isRequired"
+      />
+      <ErrorMessage name="firstName" />
+      <Field
+        name="lastName"
+        type="text"
+        placeholder="Last Name"
+        :rules="isRequired"
+      />
+      <ErrorMessage name="lastName" />
+      <Field
+        name="email"
+        type="email"
+        placeholder="Email"
+        :rules="validateEmail"
+      />
+      <ErrorMessage name="email" />
+      <Field
+        name="telNumber"
+        type="tel"
+        placeholder="Contact Number"
+        :rules="isRequired"
+      />
+      <ErrorMessage name="telNumber" />
+      <Field
+        as="textarea"
         name="message"
         id=""
         cols="30"
         rows="10"
         placeholder="Message"
-      ></textarea>
+        :rules="isRequired"
+      />
+      <ErrorMessage name="message" />
       <div class="form-btn">
         <input class="" type="button" value="&nbsp;" />
-        <input class="" type="button" value="Make Request" />
+        <button>Make Request</button>
       </div>
-    </form>
+    </Form>
   </div>
 </template>
+
+<script lang="ts">
+import { Form, Field, ErrorMessage } from "vee-validate";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
+  methods: {
+    onSubmit(values: any) {
+      console.log(JSON.stringify(values, null, 2)); //Send the data to the database/ API
+
+      (this.$refs.form as any).resetForm();
+    },
+    isRequired(value: any) {
+      if (!value) {
+        return "The field should not be empty";
+      }
+      return true;
+    },
+    validateEmail(value: any) {
+      if (!value) {
+        return "This field should be empty";
+      }
+      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+      if (!regex.test(value)) {
+        return "This field must contain a valid email";
+      }
+      return true;
+    },
+  },
+});
+</script>
