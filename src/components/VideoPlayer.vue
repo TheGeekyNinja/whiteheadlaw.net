082dba0dcbdea4c9b9b063f27120ca44bb3e3fd5
Dindz <dindoquitor@gmail.com>
2022-06-08 16:50:44 +0800

    Update the site per instructions of graham.

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>
+      <video controls>
+        <source
+          type="video/mp4"
+          src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
+        />
+      </video>
+    </div>
+  </div>
+</template>
---
+++ src/pages/Video.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="player">
+      <div class="player-sizer">
+        <VideoPlayer />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import VideoPlayer from "../components/VideoPlayer.vue";
+  import { defineComponent } from "vue";
+  export default defineComponent({
+    name: "Video Page",
+    components: {
+      VideoPlayer,
+    },
+  });
+</script>
