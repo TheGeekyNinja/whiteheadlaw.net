db03813152b04cea23678c3d0fdd2add40bbd42f
Dindz <dindoquitor@gmail.com>
2022-10-11 20:14:26 +0800

    Update the site per instruction from planka.

---
+++ src/pages/IndemnityFund.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="indemnityfund-page">
+      <h1>Special Indemnity Fund</h1>
+      <div>
+        <p>
+          Have you or a family member been injured in an automobile accident due
+          to the negligence of another driver? Do you have questions on how
+          personal injury cases work and what you may be entitled to? The video
+          below will give you answers to some of those questions directly from
+          attorney Richard Whitehead.
+        </p>
+        <p>
+          Have you lost a family member due to the negligence of someone else.
+          Do you need help in evaluating your case and your rights. Below is a
+          video that will help explain the process as described by attorney
+          Richard Whitehead who has 41 years of experience practicing injury
+          law.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+m
---
+++ src/pages/WorksMan.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div class="worksman-page">
+      <h1>Workman's Compensation</h1>
+      <div class="worksman-desc">
+        <p>
+          The video below will give you information directly from the attorney
+          on how he proceeds with an Idaho Workers Compensation case along with
+          what you may be entitled to as an injured worker.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
